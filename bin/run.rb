d4e5bafe6437276038f0a6735fd2f21751d294e0
SchruteFarms23 <romanmullady@gmail.com>
2017-08-02 13:34:37 -0400

    updated fav_player method

--- bin/run.rb
+++ bin/run.rb
@@ -14,5 +14,23 @@ new_cli.fav_player
 # new_cli.get_size
 
 new_cli.get_pricerange
-
 new_cli.here_suggestions
+
+
+# def run_main
+#   new_cli = Cli.new
+#
+#   if new_cli.login_or_create_account == 'returning customer'
+#     run_existing_user
+#   elsif new_cli.login_or_create_account == "new_customer"
+#     run_new_user
+#
+# end
+#
+# def run_new_user
+# end
+#
+# def run_existing_user
+# end
+#
+# run_main
--- lib/cli.rb
+++ lib/cli.rb
@@ -13,7 +13,7 @@ class Cli
 
   def login_or_create_account
       puts "Hi welcome to Nike! Have you used our service before? Please type 'yes' or 'no'"
-      binding.pry
+      # binding.pry
       response = gets.chomp.downcase
       if response == "yes"
         puts "Please type in your username"
@@ -21,6 +21,7 @@ class Cli
         checking_customer = Customer.find_by(name: username)
               if checking_customer
                 @new_customer = checking_customer
+                #move on to next method
               else
                 puts "User does not exist. Try again? 'y' 'n'"
                 response = gets.chomp.downcase
@@ -79,12 +80,25 @@ end #what sports are you interested in?
 
   def fav_player #if sport matches player sport_id display players
     puts "Here are a list of athletes for the sport(s) you've chosen."
-    @new_customer.sports.each {|sports| puts sportss.sport} #We have some shoes by some basketball players
+    # binding.pry
+    arr = @new_customer.sports.map {|sportt| sportt.id}
+    arr.each do |id|
+      Player.where("sport_id = #{id}").each_with_index {|player,idx| puts "#{player.sport.sport}: #{idx+1}.#{player.name}"}
+    end
+    puts "Please pick a player from the list by typing in the name."
+    input = gets.chomp.downcase
+    found_player = Player.find_by(name: input)
+    if found_player
+      PlayerAssociation.create(customer_id:@new_customer.id, player_id: found_player.id)
+    else
+      puts "Please pick a player from the list by typing in the name"
+      fav_player
+    end
+  end
+
+     #We have some shoes by some basketball players
     #lists players numbered based on the sports that they choose. asks user to type number corresponding to the player
     #saves player_id to user's favorite players. Customer has many players
-     player = gets.chomp
-     @new_customer.player_id = Player.find_by(name: player).id
-  end
 
   # def get_size
   #   puts "What's your shoe size?"
@@ -106,7 +120,19 @@ end #what sports are you interested in?
     else
       puts @new_customer.cart
     end
+    loop do
+    puts "Do you want to look at shoes from another player? 'yes' or 'no'"
+    response = gets.chomp
+    if response == "yes"
+      fav_player
+    elsif response == "no"
+      puts "Thanks for visiting us at Nike.com"
+      break
+    else
+      puts "Invalid response"
   end
+end
+end
 
   ##asdf
 
